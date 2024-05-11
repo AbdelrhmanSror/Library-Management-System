14021edfffc7bbad38102f1a6fdd646219769f5f
abdelrhman sror <sror12347@gmail.com>
2024-05-11 23:29:06 +0300

    Add Book Entity: ID, Title, Author, Pub Year and  ISBN.

---
+++ src/main/java/com/example/librarymanagementsystem/entities/Book.java
@@ -0,0 +1,27 @@
+package com.example.librarymanagementsystem.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+
+}
