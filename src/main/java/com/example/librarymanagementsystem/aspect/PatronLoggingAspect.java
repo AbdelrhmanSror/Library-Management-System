dfe4ab39f50d2fb7384cf862de64b78578acd2d2
abdelrhman sror <sror12347@gmail.com>
2024-05-12 16:11:37 +0300

    adding logging aspect for books and patrons and borrowing record

---
+++ src/main/java/com/example/librarymanagementsystem/aspect/PatronLoggingAspect.java
@@ -0,0 +1,31 @@
+package com.example.librarymanagementsystem.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class PatronLoggingAspect {
+
+
+    @Before("execution(* com.example.librarymanagementsystem.services.PatronService.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        log.info("Method execution started: {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.example.librarymanagementsystem.services.PatronService.*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        log.info("Method execution completed: {}", joinPoint.getSignature());
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.example.librarymanagementsystem.services.PatronService.*(..))", throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+        log.error("Exception thrown in method {}: {}", joinPoint.getSignature(), exception.getMessage());
+    }
+}
